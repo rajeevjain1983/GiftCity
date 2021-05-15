11ed53331c382a0260b822d059494c364d3e4b00
Rajeev Jain <rajeev.jain@publicissapient.com>
2021-05-16 01:41:02 +0530

    enable CORS

--- src/service/axiosGC.js
+++ src/service/axiosGC.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+// axios.defaults.withCredentials = true
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 const axiosGC = axios.create({
   // baseURL: "http://my-json-server.typicode.com/rajeevjain1983/GiftCity",
   baseURL: "http://192.168.1.6:4000/",
